41a7e53d770d054444f54915da52654d97c392b5
Miguel Angel <m.zapes@gmail.com>
2015-08-06 17:17:04 +0200

    Edit helper file
    Adding getRandomImage function and correct getBrands function

--- mod_randombrand/helper.php
+++ mod_randombrand/helper.php
@@ -9,27 +9,43 @@
 
 class modRandomBrandHelper
 {
-    /**
-     * Retrieves the hello message
-     *
-     * @param   array  $params An object containing the module parameters
-     *
-     * @access public
-     */
-    public static function getBrands($params)
-    {
-      // Obtain a database connection
-      $db = JFactory::getDbo();
-      // Retrieve the shout
-      $query = $db->getQuery(true)
-                  ->select($db->quoteName('id, brand_name, published, logo, webpage'))
-                  ->from($db->quoteName('#__brand'))
-                  ->where('published = true');
-      // Prepare the query
-      $db->setQuery($query);
-      // Load the row.
-      $result = $db->loadResult();
-      // Return the Hello
-      return $result;
-    }
+   /**
+   * Retrieves the list images
+   *
+   * @access public
+   */
+   public static function getBrands()
+   {
+   // Obtain a database connection
+   $db = JFactory::getDbo();
+   // Retrieve the shout
+   $query = $db->getQuery(true)
+               ->select($db->quoteName(array('id', 'brand_name', 'published', 'logo', 'webpage')))
+               ->from($db->quoteName('#__brand'))
+               ->where($db->quoteName('published') . '= 1 ');
+   // Prepare the query
+   $db->setQuery($query);
+   // Load the results as a list of stdClass objects.
+   $results = $db->loadObjectList();
+   // Return the Hello
+   return $results;
+   }
+
+   /**
+	 * Retrieves a random image
+	 *
+	 * @param   \Joomla\Registry\Registry  &$params  module parameters object
+	 * @param   array                      $images   list of images
+	 *
+	 * @return  mixed
+	 */
+	public static function getRandomImage(&$params, $images)
+	{
+      $random = array_rand($images, 12);
+      foreach ($random as $key) {
+         $img[] = $images[$key];
+      }
+
+      return $img;
+   }
 }
