3c7d48ffdee0fbda98828c0e58c7006c63505fcf
Miguel Angel <m.zapes@gmail.com>
2015-08-06 10:54:04 +0200

    Create file structure

---
+++ mod_randombrand/helper.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  mod_randombrand
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+class modRandomBrandHelper
+{
+    /**
+     * Retrieves the hello message
+     *
+     * @param   array  $params An object containing the module parameters
+     *
+     * @access public
+     */
+    public static function getBrands($params)
+    {
+        return 'Hello!';
+    }
+}
---
+++ mod_randombrand/mod_randombrand.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package     Joomla.Bladis
+ * @subpackage  mod_randombrand
+ *
+ * @copyright   Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+ * @license     GNU General Public License version 2 or later; see LICENSE.txt
+ */
+
+// No direct access
+defined('_JEXEC') or die;
+// Include the syndicate functions only once
+require_once dirname(__FILE__) . '/helper.php';
+
+$random_img = modRandomBrandHelper::getBrands($params);
+require JModuleHelper::getLayoutPath('mod_randombrand');
